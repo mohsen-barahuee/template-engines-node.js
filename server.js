25e9cdd6bcc6a63c4e5014c9479438a18500b950
Mohsen barahuee <nakel3920@gmail.com>
2025-02-24 14:26:19 +0330

    pug template

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require("path")
+const app = express()
+
+app.set("view engine", 'pug')
+app.set("views", path.join(__dirname, "views"))
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+app.listen(4001, () => {
+    console.log("Server is Runing on port 4001");
+
+})
+
+
+
+
+
+
