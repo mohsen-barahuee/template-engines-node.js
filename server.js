742323251af9ec82c21910fc962a349024a9ae44
Mohsen barahuee <nakel3920@gmail.com>
2025-02-24 14:46:57 +0330

    add css and link i

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require("path")
 const app = express()
-
+app.use('/static', express.static('public'))
 app.set("view engine", 'pug')
 app.set("views", path.join(__dirname, "views"))
+
 
 app.get('/', (req, res) => {
     res.render("index")
